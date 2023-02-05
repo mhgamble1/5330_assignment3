ba9b8d32155670111799e5af94e40c4a6e49ebfe
mhgamble1 <mhgamble1@gmail.com>
2023-02-05 17:06:22 -0600

    set background image dynamically

--- 5330_assignment3/ViewController.swift
+++ 5330_assignment3/ViewController.swift
@@ -22,6 +22,13 @@ class ViewController: UIViewController {
         formatter.dateFormat = "EEE, d MMM yyyy HH:mm:ss"
         DateTime.text = formatter.string(from: date)
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in self.updateDateTime() })
+        // set the background image to day.jpg if it is day time and night.jpg if it is night time
+        let hour = Calendar.current.component(.hour, from: date)
+        if hour >= 6 && hour < 18 {
+            self.view.backgroundColor = UIColor(patternImage: UIImage(named: "day.jpg")!)
+        } else {
+            self.view.backgroundColor = UIColor(patternImage: UIImage(named: "night.jpg")!)
+        }
     }
 
     func updateDateTime() {
